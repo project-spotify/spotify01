21333152e03fc7b9df76159f826ff59236047f09
ahmedchammakhii <ahmedchammakhi2@gmail.com>
2022-12-08 11:32:16 -0700

    ahlaé

--- js/main.js
+++ js/main.js
@@ -10,3 +10,48 @@ $(document).ready(function(){
     })
 
 })
+var musics=[];
+function Music (name,artist,album,path,img) 
+{
+var obj= {}
+obj.name=name ;
+obj.artist=artist ; 
+obj.album = album ;
+obj.added= new Date();
+obj.path = path;
+obj.img=img;
+obj.add = function () {
+    musics.push(this);
+}
+
+return obj 
+}
+var music1 = Music("poinetless","Lewis Capaldi","Single","music/music1.mp3","images/img1.jpg")
+music1.add();
+
+for (var i=0 ; i<musics.length ; i++) {
+    var tag= `<div class="containermusic" id=${i}>
+    <div class="pic">
+        <img src = "${musics[i].img}">
+    </div>
+    <div class="name">
+        <h2>${musics[i].name}</h2>
+    </div>
+    <div class="album">${musics[i].album}</div>
+</div>
+
+</div>`
+$(".about").append(tag);
+}
+
+
+
+
+$(".containermusic").click(function() {
+    id = this.id ;
+    console.log(musics[id].link)
+    audio = new Audio(musics[id].link);
+    audio.play();
+
+
+})
